2421084cc9be9eed5a1295df902b75cd0c12344f
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-23 13:24:33 -0400

    Add project files.

---
+++ Lab10/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+        private double radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public string CalculateFormattedCircumference()   //calcFormattedCirc() -> formatNumber( ->  calcCirc() ) return!
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public string CalculateFormattedArea()            //CalcFormattedArea() -> formatNumber( -> CalcArea() ) return!
+        {
+            return FormatNumber(CalculateArea());
+        }
+
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x,2).ToString();
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static int RUN(int count)
+        {
+            string c = "";
+            Console.Write("\nEnter radius: ");
+            if(!double.TryParse(Console.ReadLine(), out double temp))
+            {
+                Console.WriteLine("BAD NUBMER");
+                return RUN(count);
+            }
+            else
+            {
+            Circle newCircle = new Circle(temp);
+            Console.WriteLine("CIRCUMFREENCE: " + newCircle.CalculateFormattedCircumference());
+            Console.WriteLine("AREA: " + newCircle.CalculateFormattedArea());
+            Console.Write("enter 'n' to go again: ");
+            c = Console.ReadLine().ToLower();
+
+            if (c != "n")
+                return RUN(count)+1;
+            }
+            return count+1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nYou created " + RUN(0) + " circles, sick.");
+        }
+    }
+}
